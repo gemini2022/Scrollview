b04126234c925d0e28eee6f8f7208abd6ddca75d
Bron LaPoint <gemini2022@yahoo.com>
2024-06-30 12:44:43 -0400

    published scrollview to verdaccio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,19 @@
-import { Component, ElementRef, Renderer2, inject, viewChild } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { ScrollbarButtonComponent, ScrollbarComponent, ScrollbarThumbComponent, ScrollbarTrackComponent, ScrollviewComponent } from 'scrollview';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    ScrollviewComponent,
     ScrollbarComponent,
+    ScrollviewComponent,
     ScrollbarTrackComponent,
+    ScrollbarThumbComponent,
     ScrollbarButtonComponent,
-    ScrollbarThumbComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Scrollview';
-  private square = viewChild<ElementRef<HTMLElement>>('square');
-  private renderer = inject(Renderer2)
-
-  onChangeColor() {
-    document.documentElement.style.setProperty('--scrollbar-track-background-color', 'linear-gradient(white, blue)');
-  }
-
-  onVerticalOverflow() {
-    this.renderer.setStyle(this.square()?.nativeElement, 'top', '600px');
-    this.renderer.setStyle(this.square()?.nativeElement, 'left', '0');
-  }
-
-
-  onHorizontalOverflow() {
-    this.renderer.setStyle(this.square()?.nativeElement, 'left', '600px');
-    this.renderer.setStyle(this.square()?.nativeElement, 'top', '0');
-  }
-
-
-  onDoubleOverflow() {
-    this.renderer.setStyle(this.square()?.nativeElement, 'top', '600px');
-    this.renderer.setStyle(this.square()?.nativeElement, 'left', '600px');
-  }
-
-
-  onNoOverflow() {
-    this.renderer.setStyle(this.square()?.nativeElement, 'top', '0');
-    this.renderer.setStyle(this.square()?.nativeElement, 'left', '0');
-  }
-
-
-
-  onSetFocus() {
-    this.square()?.nativeElement.focus();
-  }
 }
